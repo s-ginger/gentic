af924a6f22541da908c5bbcd96c197798fcac923
s-ginger <jum80frol@gmail.com>
2026-09-16 19:30:44 +0600

    modified:   memory/memory.go

--- memory/memory.go
+++ memory/memory.go
@@ -1,4 +1,19 @@
 package memory
 
+import (
+	"context"
 
+	"github.com/s-ginger/gentic/graph"
+	"github.com/s-ginger/gentic/llm"
+)
+
+type Memory interface {
+	Load(ctx context.Context) (graph.State, error)
+
+	Save(ctx context.Context, state graph.State) error
+
+	AddMessage(ctx context.Context, message llm.Message) error
+
+	Messages(ctx context.Context) ([]llm.Message, error)
+}
 
