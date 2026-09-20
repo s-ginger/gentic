1e3f9ad5ff41976cc389d150058dbdfa0c13d185
s-ginger <jum80frol@gmail.com>
2026-09-20 17:22:50 +0600

    modified:   logger/logger.go

--- logger/logger.go
+++ logger/logger.go
@@ -3,13 +3,72 @@ package logger
 import (
 	"context"
 	"log/slog"
+
+	"github.com/s-ginger/gentic/graph"
 )
 
-type Logger interface {
-	Log(
-		ctx context.Context,
-		level slog.Level,
-		msg string,
-		args ...any,
+type Hook struct {
+	logger *slog.Logger
+}
+
+func New(logger *slog.Logger) *Hook {
+	return &Hook{
+		logger: logger,
+	}
+}
+
+func (h *Hook) BeforeRun(
+	ctx context.Context,
+	run graph.RunContext,
+) error {
+	h.logger.InfoContext(ctx, "graph started")
+
+	return nil
+}
+
+func (h *Hook) AfterRun(
+	ctx context.Context,
+	run graph.RunContext,
+) error {
+	h.logger.InfoContext(ctx, "graph finished")
+
+	return nil
+}
+
+func (h *Hook) BeforeNode(
+	ctx context.Context,
+	node graph.NodeContext,
+) error {
+	h.logger.InfoContext(
+		ctx,
+		"node started",
+		slog.String("node", node.Name),
+	)
+
+	return nil
+}
+
+func (h *Hook) AfterNode(
+	ctx context.Context,
+	node graph.NodeContext,
+) error {
+	h.logger.InfoContext(
+		ctx,
+		"node finished",
+		slog.String("node", node.Name),
+	)
+
+	return nil
+}
+
+func (h *Hook) OnError(
+	ctx context.Context,
+	errContext graph.ErrorContext,
+) {
+	h.logger.ErrorContext(
+		ctx,
+		"graph error",
+		slog.String("node", errContext.Node),
+		slog.Any("error", errContext.Err),
 	)
 }
